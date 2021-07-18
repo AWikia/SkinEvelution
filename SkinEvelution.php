369e9b6a31d950e550d7fdaf0b87a7bb99a8e026
HM The Mister Techno <giorgos4006@gmail.com>
2021-07-18 17:39:51 +0300

    Add files via upload (#1)

--- SkinEvelution.php
+++ SkinEvelution.php
@@ -2,7 +2,6 @@
 
 use SiteStats;
 
-
 class SkinEvelution extends SkinMustache {
     /**
      * Extends the getTemplateData function to add a template key 'html-myskin-hello-world'
@@ -14,6 +13,11 @@ class SkinEvelution extends SkinMustache {
         $data["html-titlealt"] =  $this->getOutput()->getTitle(); // Page Title
         $data["html-articlecount"] = SiteStats::articles();
         $data["html-filecount"] = SiteStats::images();
+        $data["html-rc-link"] = Title::newFromText( 'Special:RecentChanges' )->getLocalURL();
+        $data["html-files-link"] = Title::newFromText( 'Special:NewFiles' )->getLocalURL();
+        $data["html-css-link"] = Title::newFromText( 'MediaWiki:Common.css' )->getLocalURL();
+        $data["html-js-link"] = Title::newFromText( 'MediaWiki:Common.js' )->getLocalURL();
+        $data["html-spages-link"] = Title::newFromText( 'Special:SpecialPages' )->getLocalURL();
         return $data;
     }
 }
