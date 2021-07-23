fae2b7cd5e5ca9bd3dc973ba7fa8e66f94c803b7
lakejason0 <36039861+lakejason0@users.noreply.github.com>
2021-07-23 21:12:44 +0800

    Update SkinEvelution.php

--- SkinEvelution.php
+++ SkinEvelution.php
@@ -14,7 +14,7 @@ class SkinEvelution extends SkinMustache {
         $data["html-articlecount"] = SiteStats::articles();
         $data["html-filecount"] = SiteStats::images();
         $data["msg-eve-pages"] = wfMessage( 'eve-pages' )->numParams( SiteStats::articles() )->parse(); //Overwrite the message so it respects plural forms
-        $data["msg-eve-files"] = wfMessage( 'eve-files' )->numParams( SiteStats::articles() )->parse();
+        $data["msg-eve-files"] = wfMessage( 'eve-files' )->numParams( SiteStats::images() )->parse();
         $data["html-rc-link"] = Title::newFromText( 'Special:RecentChanges' )->getLocalURL();
         $data["html-files-link"] = Title::newFromText( 'Special:NewFiles' )->getLocalURL();
         $data["html-css-link"] = Title::newFromText( 'MediaWiki:Common.css' )->getLocalURL();
