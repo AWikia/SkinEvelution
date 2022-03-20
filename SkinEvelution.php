bf63a02bb55d9f824789cdf2c85a0fb1d0aae5f2
HM100 <giorgos4006@gmail.com>
2022-03-20 23:32:57 +0200

    Evelution 70.0.0
    Introduces the Servelution 2.0.0 (Vanadium) emulation
    Blocks the skin from running in 1.x.x Servelution clients

--- SkinEvelution.php
+++ SkinEvelution.php
@@ -132,30 +132,34 @@ class SkinEvelution extends SkinMustache {
 		$data["has-servelution-2.3.0"] =  ($this->getConfig()->get ( 'EvelutionServelutionRelease' ) == '2.3.0');
 		$data["has-servelution-2.2.0"] =  ($this->getConfig()->get ( 'EvelutionServelutionRelease' ) == '2.2.0');
 		$data["has-servelution-2.1.0"] =  ($this->getConfig()->get ( 'EvelutionServelutionRelease' ) == '2.1.0');
+		$data["has-servelution-2.0.0"] =  ($this->getConfig()->get ( 'EvelutionServelutionRelease' ) == '2.0.0');
+		$data["has-servelution-unsupported"] =  ( in_array($this->getConfig()->get ( 'EvelutionServelutionRelease' ), ['1.0.0','1.1.0','1.2.0','1.3.0','1.4.0'] ) );
 		// Outdated Servelution
-		$data["has-servelution-old"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"] || $data["has-servelution-3.1.0"] || $data["has-servelution-3.2.0"]);
+		$data["has-servelution-old"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"] || $data["has-servelution-3.1.0"] || $data["has-servelution-3.2.0"]);
 		// Custom Secondary Background and Text Color
-		$data["has-servelution-no-custom-2ndbg"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"] || $data["has-servelution-3.1.0"] || $data["has-servelution-3.2.0"]);
+		$data["has-servelution-no-custom-2ndbg"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"] || $data["has-servelution-3.1.0"] || $data["has-servelution-3.2.0"]);
 		// Mica/Luna Levit
-		$data["has-servelution-no-mica"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"] || $data["has-servelution-3.1.0"]);
+		$data["has-servelution-no-mica"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"] || $data["has-servelution-3.1.0"]);
 		// Image Filter
-		$data["has-servelution-no-imagefilter"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"]);
+		$data["has-servelution-no-imagefilter"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"] || $data["has-servelution-3.0.0"]);
 		// Custom Primary Font
-		$data["has-servelution-no-customfont"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"]);
+		$data["has-servelution-no-customfont"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"] || $data["has-servelution-2.8.0"]);
 		// Custom Acryllic opacity
-		$data["has-servelution-no-customacryllic"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"]);
+		$data["has-servelution-no-customacryllic"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"] || $data["has-servelution-2.7.0"]);
 		// Luna Accent (Unfocus)
-		$data["has-servelution-no-newluna"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"]);
+		$data["has-servelution-no-newluna"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"] || $data["has-servelution-2.6.0"]);
 		// Luna Accent
-		$data["has-servelution-no-accent"] = ( $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"]);
+		$data["has-servelution-no-accent"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"] || $data["has-servelution-2.5.0"]);
 		// Dynamic Generic Colors
-		$data["has-servelution-no-dgeneric"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"]);
+		$data["has-servelution-no-dgeneric"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"] || $data["has-servelution-2.4.0"]);
 		// Floating Actions
-		$data["has-servelution-no-actions"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"]);
+		$data["has-servelution-no-actions"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"] || $data["has-servelution-2.3.0"]);
 		// Custom Header Color
-		$data["has-servelution-no-headertext"] = ($data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"]);
+		$data["has-servelution-no-headertext"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"] || $data["has-servelution-2.2.0"]);
 		// Dual Foreground Color
-		$data["has-servelution-no-dground"] = ($data["has-servelution-2.1.0"]);
+		$data["has-servelution-no-dground"] = ($data["has-servelution-2.0.0"] || $data["has-servelution-2.1.0"]);
+		// Vanadium
+		$data["has-servelution-vanadium"] = ($data["has-servelution-2.0.0"]);
 		/* Theming Stuff */
 		$data["data-theme-A"] = $this->getConfig()->get ( 'EvelutionThemeA' );
 		$data["data-theme-B"] = $this->getConfig()->get ( 'EvelutionThemeB' );
